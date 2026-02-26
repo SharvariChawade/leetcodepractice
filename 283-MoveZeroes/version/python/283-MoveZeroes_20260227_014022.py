5b24372c0840bec32d2a13a461abfdc840081bd3
SharvariChawade <89578368+SharvariChawade@users.noreply.github.com>
2026-02-27 01:40:23 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/version/python/283-MoveZeroes_20260227_014022.py
@@ -0,0 +1,27 @@
+# Last updated: 27/02/2026, 01:40:22
+1class Solution:
+2    def moveZeroes(self, nums: List[int]) -> None:
+3        """
+4        Do not return anything, modify nums in-place instead.
+5        """
+6        l = 0
+7        r = 1
+8        
+9        while(r<len(nums)):
+10            if(nums[l]==0 and nums[r]!=0):
+11                temp = nums[l]
+12                nums[l] = nums[r]
+13                nums[r] = temp
+14                l += 1
+15                r += 1
+16            elif (nums[l]==0 and nums[r]==0):
+17                r += 1
+18            else:
+19                r+=1
+20                l+=1
+21            #elif(nums[l]!=0 and nums[r]==0):
+22            #    r +=1
+23            #    l += 1
+24            #elif(nums[l]!=0 and nums[r]!=0):
+25            #    r+=1
+26            #    l+=1
