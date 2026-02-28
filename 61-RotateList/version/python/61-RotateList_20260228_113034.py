973b81e2748e635231e740fffe680b5a6be50fb2
SharvariChawade <89578368+SharvariChawade@users.noreply.github.com>
2026-02-28 11:30:34 +0530

    Create 61-RotateList

---
+++ 61-RotateList/version/python/61-RotateList_20260228_113034.py
@@ -0,0 +1,17 @@
+# Last updated: 28/02/2026, 11:30:34
+1class Solution:
+2    def isPalindrome(self, s: str) -> bool:
+3        l = 0
+4        r = len(s)-1
+5        s = s.lower()
+6        while(r>l):
+7            while l<r and s[r] not in 'qwertyuiopasdfghjklzxcvbnm1234567890':
+8                r -= 1
+9            while l<r and s[l] not in 'qwertyuiopasdfghjklzxcvbnm1234567890':
+10                l+=1
+11            if(s[r]==s[l]):
+12                r-=1
+13                l+=1
+14            else:
+15                return False
+16        return True
